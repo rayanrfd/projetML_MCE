828da9b826f3a560b3de5daa81efac4cbff2cf1b
Votre Nom <vous@exemple.com>
2024-11-16 00:07:12 +0100

    Finished the bank workflow

--- notebooks/ml_workflow.py
+++ notebooks/ml_workflow.py
@@ -1,19 +1,34 @@
+# Importing the basic libraries
 import numpy as np
 import pandas as pd
 
+# Importing preprocessers
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, Normalizer, StandardScaler
-from sklearn.impute import SimpleImputer
 
+# Importing the classifiers
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import GradientBoostingClassifier
 
 
 # One function for pre-processing
+
+## Kidney dataset '?' handling
+def question_mark_handling(data):
+    for col in data.columns:
+        if data[col].dtype() == 'string':
+              for m in data[col]:
+                    if m[0:1]=='\t':
+                        m = m[2:]
+    return data
+    
+    data = data.replace(['?', '\t', '\t?'], np.nan)
+    return data
+
+## Divides the dataset between the features and the label
 def extract_label(data, column):
-    data = data.drop(column, axis=1)
     label = data[column]
+    data = data.drop(column, axis=1)
     return data, label
 
 
@@ -22,18 +37,19 @@ def data_preprocessing_impute(data): #mean_col, freq_col, uknw_col, value):
     mean_col = data.select_dtypes(include=['int64', 'float64']).columns.to_list()
     if mean_col is not None:
         data[mean_col] = data[mean_col].fillna(data[mean_col].mean())
+
     freq_col = data.select_dtypes(include=['object']).columns.to_list()
     if freq_col is not None:
         data[freq_col] = data[freq_col].fillna(data[freq_col].mode())
 
     return data
 
-def data_preprocessing_encode(data): #, num_col, cat_col, ord_col):
 
+def data_preprocessing_encode(data): #, num_col, cat_col, ord_col):
      
     num_col = data.select_dtypes(include=['int64', 'float64']).columns.to_list()
     if num_col is not None:
-        data[num_col] = (data[num_col] - data[num_col].mean()) / data[num_col].std()
+        data[num_col] = (data[num_col] - data[num_col].min()) / (data[num_col].max() - data[num_col].min())
 
     cat_col = data.select_dtypes(include=['object']).columns.to_list()
     if cat_col is not None: 
@@ -42,6 +58,7 @@ def data_preprocessing_encode(data): #, num_col, cat_col, ord_col):
     return data
 
 def data_preprocessing(data, label_column): #, num_col, cat_col, ord_col, mean_col, freq_col, uknw_col, value):
+    data.drop_duplicates(inplace=True)
     data, label = extract_label(data, label_column)
     imputed_data = data_preprocessing_impute(data)
     preprocessed_data = data_preprocessing_encode(imputed_data)
@@ -50,11 +67,12 @@ def data_preprocessing(data, label_column): #, num_col, cat_col, ord_col, mean_c
 
 # One function to prepare the dataset for training
 def prepare_dataset(data, label):
-    X, y = np.array(data), np.array(label)
-    X_train, X_, y_train, y_ = train_test_split(X, y, test_size=0.2)
-    X_val, X_test, y_val, y_test = train_test_split(X_, y_, test_size=0.1)
+    X = np.array(data)
+    y = np.array(label)
+    print(y.shape)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-    return X_train, X_val, X_test, y_train, y_val, y_test
+    return X_train, X_test, y_train, y_test
 
 # One function for training (typically applies up to 5 different methods for binary classification)
 def training_validating_model(X_train, y_train, X_test, y_test, classifier):
